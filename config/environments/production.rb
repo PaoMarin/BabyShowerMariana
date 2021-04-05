89e7afdc25224552ec3f69f83a849dd4d76993a7
PaoMarin <paomg.9614@gmail.com>
2021-04-05 04:43:28 -0600

    Archisvos conf

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,6 +87,17 @@ Rails.application.configure do
   # Use a different logger for distributed setups.
   # require "syslog/logger"
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
+  config.action_mailer.default_url_options = {host: 'https://babyshower-mariana.herokuapp.com/'}
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "mail.google.com", ####important
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: 'credit.challenge.app@gmail.com',
+    password: 'xcw0nhvw'
+}
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
