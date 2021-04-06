a3e02de429c01f4c0740aa3beaa4ee0018ad8921
PaoMarin <paomg.9614@gmail.com>
2021-04-05 20:48:22 -0600

    style

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'joskamarin@gmail.com', cc: 'paomg.9614@gmail.com'
+  default from: 'credit.challenge.app@gmail.com', cc: 'paomg.9614@gmail.com'
   layout 'mailer'
 end
 
